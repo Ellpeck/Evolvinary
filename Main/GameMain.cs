398c130e436f6f9d6b9dbfac345c16ef2082bbcc
Ellpeck <megamaximal@gmail.com>
2016-09-04 01:27:16 +0200

    Some changes to what gets commited and what doesn't. Also 4 spaces instead of tabs

--- Main/GameMain.cs
+++ Main/GameMain.cs
@@ -2,25 +2,25 @@
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Evolvinary.Main{
-	public class GameMain : Game{
-		public GraphicsDeviceManager Graphics;
-		public SpriteBatch SpriteBatch;
+    public class GameMain : Game{
+        public GraphicsDeviceManager Graphics;
+        public SpriteBatch SpriteBatch;
 
-		public GameMain(){
-			this.Graphics = new GraphicsDeviceManager(this);
-			this.Content.RootDirectory = "Content";
+        public GameMain(){
+            this.Graphics = new GraphicsDeviceManager(this);
+            this.Content.RootDirectory = "Content";
 
-			this.Graphics.PreferredBackBufferWidth = 1280;
-			this.Graphics.PreferredBackBufferHeight = 720;
-		}
+            this.Graphics.PreferredBackBufferWidth = 1280;
+            this.Graphics.PreferredBackBufferHeight = 720;
+        }
 
-		protected override void LoadContent(){
-			this.SpriteBatch = new SpriteBatch(this.GraphicsDevice);
-		}
+        protected override void LoadContent(){
+            this.SpriteBatch = new SpriteBatch(this.GraphicsDevice);
+        }
 
-		protected override void Draw(GameTime gameTime){
-			this.GraphicsDevice.Clear(Color.CornflowerBlue);
-			base.Draw(gameTime);
-		}
-	}
+        protected override void Draw(GameTime gameTime){
+            this.GraphicsDevice.Clear(Color.CornflowerBlue);
+            base.Draw(gameTime);
+        }
+    }
 }
--- Main/World/Cell.cs
+++ Main/World/Cell.cs
@@ -1,14 +1,13 @@
 ﻿namespace Evolvinary.Main.World{
-	public class Cell{
+    public class Cell{
+        public World World;
+        public int PosX;
+        public int PosY;
 
-		public World World;
-		public int PosX;
-		public int PosY;
-
-		public Cell(World world, int posX, int posY){
-			this.World = world;
-			this.PosX = posX;
-			this.PosY = posY;
-		}
-	}
+        public Cell(World world, int posX, int posY){
+            this.World = world;
+            this.PosX = posX;
+            this.PosY = posY;
+        }
+    }
 }
